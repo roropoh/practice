95d5d4d7dd443261691d60d11cb9433ecd3d4552
Robin Hsieh <hsiehrobin@gmail.com>
2018-05-03 11:52:39 -0700

    adding chapter 3 start

---
+++ ctci/c03_stacksandqueues/p0.py
@@ -0,0 +1,8 @@
+'''
+P0. Implementing a Stack and a Queue
+'''
+
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.next = None # the pointer initially points to nothing
