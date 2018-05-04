18e9c2ca38dce15574751267ee4e441a3ce9b997
Robin Hsieh <Robin.Hsieh@ge.com>
2018-05-03 22:41:57 -0700

    completed 1.4

--- ctci/c01_arrayandstrings/1.4.py
+++ ctci/c01_arrayandstrings/1.4.py
@@ -1,5 +1,28 @@
 def palindrome(str):
-    print "palindrome({0}, {1})".format(str1, str2)
-    hashmap = {}
+    print "palindrome({0})".format(str)
+    table = getCountPerCharacter(str)
+    return checkMaxOneOddinTable(table)
 
-palindrome("tact coa")
+def checkMaxOneOddinTable(table):
+    foundOdd = False
+    for i in table:
+        if table[i] % 2 == 1:
+            if foundOdd == True:
+                return False
+            foundOdd = True
+    return True
+
+def getCountPerCharacter(str):
+    hashmap = {}
+    for i in str:
+        if i.isalpha():
+            if i in hashmap:
+                hashmap[i] += 1
+            else:
+                hashmap[i] = 1
+    return hashmap
+
+print palindrome("pale, ple")
+print palindrome("pales, pale")
+print palindrome("pale, bale")
+print palindrome("pale, bae")
