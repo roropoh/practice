f750043944d3efb6c9fad6aa50158647e0bbacb4
Robin Hsieh <Robin.Hsieh@ge.com>
2018-05-07 17:09:37 -0700

    adding 1.9 question

--- ctci/c01_arrayandstrings/1.7.py
+++
@@ -1,4 +0,0 @@
-# rotate matrix:
-# given an image represented by an NxN matrix, where each pixel in the image
-# is 4 bytes, write a methood to rotate the image by 90 degrees. can you do this
-# in place?
